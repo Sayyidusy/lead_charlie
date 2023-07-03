ef14c89c52151336218689e5967380045fa7d52e
ashenladd <akifrh24@gmail.com>
2023-07-03 23:41:37 +0800

    Integrating Firebase

--- app/src/main/java/com/example/charlie/ui/LoginFragment.kt
+++ app/src/main/java/com/example/charlie/ui/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.charlie.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,16 +9,20 @@ import android.view.ViewGroup
 import com.example.charlie.R
 import com.example.charlie.databinding.FragmentBannerRateCardBinding
 import com.example.charlie.databinding.FragmentLoginBinding
+import com.google.firebase.firestore.Source
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
     private var _binding : FragmentLoginBinding? = null
 
     private val binding get() = _binding!!
+    val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentLoginBinding.inflate(layoutInflater)
         return binding.root
     }
@@ -25,9 +30,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply{
-            clAudiens.setOnClickListener{
 
-            }
         }
     }
 
