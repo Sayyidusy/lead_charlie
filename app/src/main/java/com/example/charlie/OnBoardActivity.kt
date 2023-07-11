8484e29b69a3d047abdea9bdce668372d5447ed7
Sayyidusy <sayyidusyalghiffari@gmail.com>
2023-07-11 23:27:31 +0700

    login register flow

---
+++ app/src/main/java/com/example/charlie/OnBoardActivity.kt
@@ -0,0 +1,28 @@
+package com.example.charlie
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.charlie.databinding.ActivityOnboardBinding
+import com.example.charlie.ui.authentication.LoginActivity
+import com.example.charlie.ui.authentication.RegisterActivity
+
+class OnBoardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.clAudiens.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.clKreator.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
